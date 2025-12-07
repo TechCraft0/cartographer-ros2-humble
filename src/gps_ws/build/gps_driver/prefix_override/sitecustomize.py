88c5a0aa417b092610f7bc43bd629316a1d8dfd4
TechCraft0 <onlywl9598@gmail.com>
2025-12-07 15:44:10 +0800

    delete

--- src/gps_ws/build/gps_driver/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/only/workspace/wang/smartcar/gps_module/gps_ws/install/gps_driver'
--- src/imu_ws/build/wit_ros2_imu/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/only/workspace/wang/smartcar/10_axis_imu/imu_ws/install/wit_ros2_imu'
